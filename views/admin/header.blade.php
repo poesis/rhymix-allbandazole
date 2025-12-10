430e762c5bd759166c43107bc049837f6f1a5691
Kijin Sung <kijin@kijinsung.com>
2025-12-10 12:51:11 +0900

    Remove stray quote character in admin header

--- views/admin/header.blade.php
+++ views/admin/header.blade.php
@@ -6,19 +6,19 @@
 </div>
 
 <ul class="x_nav x_nav-tabs">
-	<li @class(['x_active' => $act == 'dispAllbandazoleAdminConfig'])">
+	<li @class(['x_active' => $act == 'dispAllbandazoleAdminConfig'])>
 		<a href="@url(['module' => 'admin', 'act' => 'dispAllbandazoleAdminConfig'])">{$lang->cmd_allbandazole_general_config}</a>
 	</li>
-	<li @class(['x_active' => $act == 'dispAllbandazoleAdminCountries'])">
+	<li @class(['x_active' => $act == 'dispAllbandazoleAdminCountries'])>
 		<a href="@url(['module' => 'admin', 'act' => 'dispAllbandazoleAdminCountries'])">{$lang->cmd_allbandazole_countries}</a>
 	</li>
-	<li @class(['x_active' => $act == 'dispAllbandazoleAdminClouds'])">
+	<li @class(['x_active' => $act == 'dispAllbandazoleAdminClouds'])>
 		<a href="@url(['module' => 'admin', 'act' => 'dispAllbandazoleAdminClouds'])">{$lang->cmd_allbandazole_clouds}</a>
 	</li>
-	<li @class(['x_active' => $act == 'dispAllbandazoleAdminCaptcha'])">
+	<li @class(['x_active' => $act == 'dispAllbandazoleAdminCaptcha'])>
 		<a href="@url(['module' => 'admin', 'act' => 'dispAllbandazoleAdminCaptcha'])">{$lang->cmd_allbandazole_captcha}</a>
 	</li>
-	<li @class(['x_active' => $act == 'dispAllbandazoleAdminCustomize'])">
+	<li @class(['x_active' => $act == 'dispAllbandazoleAdminCustomize'])>
 		<a href="@url(['module' => 'admin', 'act' => 'dispAllbandazoleAdminCustomize'])">{$lang->cmd_allbandazole_customize}</a>
 	</li>
 </ul>
